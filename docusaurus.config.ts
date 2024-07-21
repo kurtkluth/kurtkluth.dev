a454d7169d8433fcd2a66ec4a51221a014eb7d60
kurtkluth <kurtkluth@me.com>
2024-07-21 11:20:01 -0700

    updated url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,14 +40,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kurtkluth/kurtkluth.dev/',
+            'https://github.com/kurtkluth/kurtkluth.dev/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/kurtkluth/kurtkluth.dev/',
+            'https://github.com/kurtkluth/kurtkluth.dev/blog/',
         },
         theme: {
           customCss: './src/css/custom.css',
